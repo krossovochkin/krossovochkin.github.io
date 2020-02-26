24eb8b6ec2bc70a018a9009257bfa2a4acd8eab6
Vasya Drobushkov <vasya.drobushkov@gmail.com>
2020-02-26 21:32:02 +0300

    added publication

--- lib/components/publications.dart
+++ lib/components/publications.dart
@@ -1,6 +1,7 @@
 import '../ui/ui_components.dart';
 
 final _data = {
+  "From RxJava to Kotlin Flow: Stream Types": "https://proandroiddev.com/from-rxjava-to-kotlin-flow-stream-types-7916be6cabc2",
   "From RxJava to Kotlin Flow: Error Handling": "https://proandroiddev.com/from-rxjava-to-kotlin-flow-error-handling-da1f6a4f2708",
   "Vinyls animation with MotionLayout": "https://medium.com/@krossovochkin/vinyls-animation-with-motionlayout-595e716c2752",
   "From RxJava to Kotlin Flow: Backpressure": "https://proandroiddev.com/from-rxjava-to-kotlin-flow-backpressure-d1fb91e6dea8",
